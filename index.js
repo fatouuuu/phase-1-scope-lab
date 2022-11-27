5ac2c174a9722904f9b54d5aa3e430e161e56205
fatouuuu <fatmaabdullrahim@gmail.com>
2022-11-27 11:48:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName="bob";
+
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    globalThis.bestCustomer="not bob"
+}
+
+function overwriteBestCustomer(){
+    globalThis.bestCustomer="maybe bob";
+} 
+
+const leastFavoriteCustomer="gael"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="bob"
+}
